9650fa013c9f3d042f81651392d04335c19f5dc5
saisankethravva <saisanketh621@gmail.com>
2023-02-20 11:46:20 -0800

    updated categories

--- backend-cultfit/src/controllers/categories.controller.js
+++ backend-cultfit/src/controllers/categories.controller.js
@@ -8,4 +8,12 @@ const router = express.Router();
 
 router.post("/", CRUD(Category).post);
 
+router.get("/", CRUD(Category).get);
 
+router.get("/:id", CRUD(Category).getOne);
+
+router.patch("/:id", CRUD(Category).update);
+
+router.delete("/:id", CRUD(Category).remove);
+
+module.exports = router;
