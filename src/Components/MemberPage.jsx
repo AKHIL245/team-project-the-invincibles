841274b72a1c84a41564005055dd4aedbd732212
AKHIL245 <akhilthaneti24@gmail.com>
2023-03-15 00:29:58 -0700

    MemberPage to handle classes

--- src/Components/MemberPage.jsx
+++ src/Components/MemberPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import "./MemberPage.css";
 function MemberPage(props) {
   const [scheduleData, setScheduleData] = useState([]);
   //const { membername } = localStorage.getItem("name");
@@ -9,7 +9,7 @@ function MemberPage(props) {
       try {
         //const { membername } = localStorage.getItem("name");
         console.log("membername",localStorage.getItem("name"));
-        const response = await fetch(`http://localhost:3000/individualschedule/${localStorage.getItem("name")}`);
+        const response = await fetch(`http://202backend-env.eba-rgmq4hxp.us-west-1.elasticbeanstalk.com/individualschedule/${localStorage.getItem("name")}`);
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
@@ -24,23 +24,33 @@ function MemberPage(props) {
   });
 
   return (
-    
-    <div>
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-      <h1>Class Schedule for {localStorage.getItem("name")}</h1>
-      <ul>
-        {scheduleData.map((classSchedule) => (
-          <li key={classSchedule._id}>
-            <p>Class: {classSchedule.classname}</p>
-            <p>Location: {classSchedule.gymaddress}</p>
-            <p>Date: {classSchedule.date}</p>
-            <p>StartTime: {classSchedule.starttime}</p>
-            <p>EndTime: {classSchedule.endtime}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="member-page-container">
+      <br></br><br></br><br></br><br></br>
+      {/* <h1 className="member-page-title">Class Schedule for {localStorage.getItem("name")}</h1> */}
+      <table className="schedule-table">
+        <thead>
+          <tr>
+            <th>Class</th>
+            <th>Location</th>
+            <th>Date</th>
+            <th>StartTime</th>
+            <th>EndTime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {scheduleData.map((classSchedule) => (
+            <tr key={classSchedule._id}>
+              <td>{classSchedule.classname}</td>
+              <td>{classSchedule.gymaddress}</td>
+              <td>{classSchedule.date}</td>
+              <td>{classSchedule.starttime}</td>
+              <td>{classSchedule.endtime}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  );  
 }
 
 export default MemberPage;
